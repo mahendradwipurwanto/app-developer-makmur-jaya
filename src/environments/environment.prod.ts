ffeff5a6da78117d5f7d88c92b6f90fa5fcae100
mahendradwipurwanto <mahendradwipurwanto@gmail.com>
2024-07-16 13:23:13 +0700

    Update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   google_client_id: '75314287440-eb60cvuejo1kta3o5p5m2crj2b3q3iv3.apps.googleusercontent.com',
-  facebook_app_id: '1744368446101227',
+  facebook_app_id: '515242771161126',
 };
